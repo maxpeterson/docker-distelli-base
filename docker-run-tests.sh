ab24cc6f9a5f4f81a62922c6f68b7a2724bdd66a
Max Peterson <max@incuna.com>
2018-07-08 22:10:44 +0100

    Remove AWS details

--- docker-run-tests.sh
+++ docker-run-tests.sh
@@ -18,10 +18,5 @@ nvm use 6
 npm install
 bower install
 grunt build
-# Test back-end
-DEFAULT_FILE_STORAGE=storages.backends.s3boto.S3BotoStorage \
-AWS_STORAGE_BUCKET_NAME=gfgp-media \
-AWS_LOCATION=gfgp-website-dev \
-AWS_ACCESS_KEY_ID=AKIAJOQABE7KQ5W4OEBQ \
-AWS_SECRET_ACCESS_KEY=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 \
+
 exec $cmd
